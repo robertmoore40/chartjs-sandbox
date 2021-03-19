562d227c85c7e78c5d40b06b46ec663d1f0afb80
BigRob <robertcmoore92@gmail.com>
2021-03-19 10:45:20 -0400

    added chart1

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ let data1 = [69, 31];
 let colors1 = ['#49A9EA', '#36CAAB'];
 
 let myDoughnutChart = document.getElementById("myChart").getContext('2d');
+
+let chart1 = new Chart(myDoughnutChart, {
+    type: 'doughnut',
